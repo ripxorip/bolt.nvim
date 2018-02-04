f178f8e6ebfe319454f47dc3e08290bf2fb5f91c
Philip Karlsson <philipkarlsson@me.com>
2018-02-04 10:16:53 +0100

    Suggested architectural structure. The files just contains garbage atm..

---
+++ rplugin/python3/bolt_tui/pane.py
@@ -0,0 +1,145 @@
+# ============================================================================
+# FILE: searcher.py
+# AUTHOR: Philip Karlsson <philipkarlsson at me.com>
+# License: MIT license
+# ============================================================================
+import os
+from vim_tc_explorer.filter import filter
+
+
+class resultGroup(object):
+    def __init__(self, fileName):
+        self.lines = []
+        self.matches = 0
+        self.fileName = fileName
+
+
+class searcher(object):
+    def __init__(self, nvim, buffer, cwd):
+        self.nvim = nvim
+        self.filter = filter()
+        self.buffer = buffer
+        # Attribute to distinguish from explorer
+        self.isSearcher = True
+        self.selected = 0
+        self.fileredFiles = []
+        self.expanded = False
+        self.cwd = cwd
+        # Header takes up 6 rows
+        self.headerLength = 6
+
+    def assignBuffer(self, buffer):
+        # This method is only called during re-init so we already have old
+        # results
+        self.buffer = buffer
+        self.prevbuffer = self.nvim.current.buffer
+        self.nvim.current.buffer = self.buffer
+        self.nvim.command('setlocal filetype=vim_tc_search_result')
+        self.nvim.current.buffer = self.prevbuffer
+        # self.createResultStructure()
+        # self.draw()
+
+    def createResultStructure(self):
+        self.results = {}
+        self.resultFiles = []
+        for line in self.buffer[1:len(self.buffer)]:
+            # Process each line
+            f = line.split(':')
+            if(f is not None):
+                if(not f[0] in self.results):
+                    self.results[f[0]] = resultGroup(f[0])
+                    self.resultFiles.append(f[0])
+                self.results[f[0]].lines.append(line)
+                self.results[f[0]].matches += 1
+
+    def getFileListFromResults(self):
+        self.fileList = []
+        self.rawFileList = []
+        for res in self.fileredFiles:
+            # Add the file
+            self.rawFileList.append(res)
+            self.fileList.append('+'+res + ' | ' +
+                                 str(self.results[res].matches) + ' matches')
+            if self.expanded:
+                for l in self.results[res].lines:
+                    self.fileList.append('  -'+l)
+                    self.rawFileList.append(l)
+
+    def search(self, dir, filePattern, inputPattern):
+        self.prevbuffer = self.nvim.current.buffer
+        self.nvim.current.buffer = self.buffer
+        self.nvim.command('setlocal filetype=vim_tc_search_result')
+        self.dir = dir
+        self.inputPattern = inputPattern
+        self.filePattern = filePattern
+        self.command = "cd %s && " % dir
+        if(not filePattern.startswith('-')):
+                filePattern = '-t' + filePattern
+        if(inputPattern is not ''):
+            self.command += "rg %s %s --vimgrep" % (filePattern, inputPattern)
+        else:
+            filePattern = filePattern.replace('-t', '-g')
+            self.command += "rg %s --files" % (filePattern)
+        self.buffer[:] = []
+        # Below does not work for mac
+        # self.nvim.command("r !\"%s\"" % self.command)
+        # Verify that this works for Windows
+        self.nvim.command("r !%s" % self.command)
+        self.nvim.current.buffer = self.prevbuffer
+        self.createResultStructure()
+        self.getFileListFromResults()
+
+    def updateListing(self, pattern):
+        self.filter.filter(self.resultFiles, pattern, self.fileredFiles)
+        self.getFileListFromResults()
+        self.changeSelection(0)
+
+    def changeSelection(self, offset):
+        # Selection is this time based on fileList
+        self.selected += offset
+        if self.selected < 0:
+            self.selected = 0
+        elif self.selected >= len(self.fileList):
+            self.selected = len(self.fileList)-1
+
+    def toggle(self):
+        self.expanded = not self.expanded
+        self.getFileListFromResults()
+
+    def draw(self):
+        self.buffer[:] = self.getUIHeader()
+        # Draw each file
+        for idx, val in enumerate(self.fileList):
+            if idx == self.selected:
+                token = '-->'
+            else:
+                token = '   '
+            self.buffer.append(token + val)
+        # Debug
+        # self.buffer.append(self.command)
+
+    def getSelected(self):
+        lineNum = None
+        currLine = self.rawFileList[self.selected]
+        if(':' in currLine):
+            # This is a match in a file
+            lineParts = currLine.split(':')
+            self.buffer[:] = lineParts
+            pathToFile = os.path.join(self.cwd, lineParts[0])
+            lineNum = int(lineParts[1])
+        else:
+            pathToFile = os.path.join(self.cwd, currLine)
+        return pathToFile, lineNum
+
+    def getUIHeader(self):
+        bar = "==============================================================="
+        leadingC = '#'
+        ret = []
+        ret.append(leadingC + bar)
+        ret.append(leadingC + 'Bolt search results')
+        # Shall be highlighted
+        ret.append(leadingC + '  $>' + self.command)
+        qhStr = '  Quik Help: <Ret>:Open <C-a>:Expand <C-q>:Quit'
+        ret.append(leadingC + qhStr)
+        ret.append(leadingC + bar)
+        return ret
---
+++ rplugin/python3/neovim_bolt_cli/neovim_bolt_cli.py
@@ -0,0 +1,129 @@
+# ============================================================================
+# FILE: explorer.py
+# AUTHOR: Philip Karlsson <philipkarlsson at me.com>
+# License: MIT license
+# ============================================================================
+import os
+import shutil
+from vim_tc_explorer.filter import filter
+
+
+class explorer(object):
+    """ Class for an explorer that is used in the panes """
+    def __init__(self, cwd):
+        self.isSearcher = False
+        # Instance of the filter
+        self.filter = filter()
+        self.cwd = cwd
+        # The the current files
+        self.currentFiles = os.listdir(self.cwd)
+        self.fileredFiles = self.currentFiles[:]
+        # Index that tracks which file that is selected
+        self.selected = 0
+        self.active = True
+        self.pattern = ''
+        # The header takes up 9 rows
+        self.headerLength = 9
+
+    def assignBuffer(self, buffer):
+        self.buffer = buffer
+
+    def draw(self):
+        explorer = self.buffer
+        # New way of getting the header
+        explorer[:] = self.getUIHeader()
+        # FIXME: Add coloring
+        for idx, val in enumerate(self.fileredFiles):
+            if idx == self.selected and self.active:
+                token = "-->"
+            else:
+                token = "   "
+            if(os.path.isdir(os.path.abspath(os.path.join(self.cwd, val)))):
+                # Folder
+                explorer.append(token + ' +' + val + '/')
+            else:
+                explorer.append(token + '  ' + val)
+
+    def rename(self, newName):
+        os.rename(self.getSelected()[0], os.path.join(self.cwd, newName))
+        self.cd('.')
+        self.updateListing(self.pattern)
+
+    def copy(self, dest):
+        selFile = self.getSelected()[0]
+        if os.path.isdir(selFile):
+            shutil.copytree(selFile, dest)
+        else:
+            shutil.copy(selFile, dest)
+        self.cd('.')
+        self.updateListing(self.pattern)
+
+    def delete(self, yesno):
+        if yesno == "y":
+            selFile = self.getSelected()[0]
+            if os.path.isdir(selFile):
+                shutil.rmtree(selFile)
+            else:
+                os.remove(selFile)
+            self.cd('.')
+            self.updateListing(self.pattern)
+
+    def move(self, dest):
+        os.rename(self.getSelected()[0], dest)
+        self.cd('.')
+        self.updateListing(self.pattern)
+
+    def mkdir(self, name):
+        os.makedirs(os.path.join(self.cwd, name))
+        self.cd('.')
+        self.updateListing(self.pattern)
+
+    def createFile(self, name):
+        open(os.path.join(self.cwd, name), 'a').close()
+        self.cd('.')
+        self.updateListing(self.pattern)
+
+    def cd(self, path):
+        self.cwd = os.path.abspath(os.path.join(self.cwd, path))
+        self.currentFiles = os.listdir(self.cwd)
+        self.fileredFiles = self.currentFiles[:]
+        self.changeSelection(0)
+
+    def updateListing(self, pattern):
+        self.pattern = pattern
+        self.filter.filter(self.currentFiles, pattern, self.fileredFiles)
+        self.changeSelection(0)
+
+    def changeSelection(self, offset):
+        self.selected += offset
+        if self.selected < 0:
+            self.selected = 0
+        elif self.selected >= len(self.fileredFiles):
+            self.selected = len(self.fileredFiles)-1
+
+    def getSelected(self):
+        pathToFile = os.path.join(self.cwd, self.fileredFiles[self.selected])
+        return pathToFile, None
+
+    # Gui header
+    def getUIHeader(self):
+        bar = "==============================================================="
+        if(self.active):
+            leadingC = '# '
+        else:
+            leadingC = '" '
+        ret = []
+        ret.append(leadingC + bar)
+        ret.append(leadingC + 'Bolt for Neovim (alpha)')
+        # Shall be highlighted
+        ret.append(leadingC + '  $>' + self.cwd)
+        qhStr = '  Quik Help: <Ret>:Open   <C-q>:Quit   <C-s>:Set CWD'
+        ret.append(leadingC + qhStr)
+        qhStr = '             <C-f>:Search <C-p>:Create File'
+        ret.append(leadingC + qhStr)
+        qhStr = '             <F2>:Rename  <F5>:Copy    <F6>:Move   '
+        ret.append(leadingC + qhStr)
+        qhStr = '             <F7>:Mkdir   <F8>:Delete   '
+        ret.append(leadingC + qhStr)
+        ret.append(leadingC + bar)
+        return ret
